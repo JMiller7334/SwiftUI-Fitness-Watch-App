41b8a8647910c28fb538aa746262cdb47c3356c2
jmiller7334 <jmiller7334@students.mchenry.edu>
2023-02-23 20:40:05 -0600

    Initial Commit

---
+++ MAD257-GymTrackerWatch Watch App/MAD257_GymTrackerWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MAD257_GymTrackerWatchApp.swift
+//  MAD257-GymTrackerWatch Watch App
+//
+//  Created by student on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MAD257_GymTrackerWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
